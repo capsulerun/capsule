3ddea209f98bcb2c062efe3e4e1a057cbead0a16
Mavdol <mdiallo.av@gmail.com>
2026-04-09 10:46:33 +0200

    remove plugin

--- integrations/elizaos-plugin/src/index.ts
+++
@@ -1,5 +0,0 @@
-import { capsulePlugin } from './plugin.ts';
-
-export { capsulePlugin } from './plugin.ts';
-export { runPython, runJavaScript, loadSandboxes } from '@capsule-run/adapter';
-export default capsulePlugin;
