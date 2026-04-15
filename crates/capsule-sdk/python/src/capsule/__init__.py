12f8fead86eec368fc2cc712b5ffb86c1e3ef60f
Mohamed Diallo <mdiallo.av@gmail.com>
2026-04-15 12:48:49 +0200

    remove urllib and http imports from python sdk initialization (#167)
    * remove urllib and http imports from python sdk initialization
    
    * bump project version to 0.8.8 and clean up Python initialization code

--- crates/capsule-sdk/python/src/capsule/__init__.py
+++ crates/capsule-sdk/python/src/capsule/__init__.py
@@ -4,13 +4,4 @@ from . import http
 from .app import TaskRunner
 from .run import run, RunnerOptions, RunnerResult, ExecutionInfo, ErrorInfo
 
-import urllib.request
-import urllib.parse
-import urllib.error
-import urllib.response
-import http.client
-import http.cookiejar
-
 exports = app.exports
-
-
